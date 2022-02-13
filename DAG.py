60b4fcc2d5b72925abb769d8d85ee159f9694b45
Shikha Dixit <shikhadixit@Shikhas-MacBook-Air.local>
2022-02-13 01:37:22 -0800

    DAG generation and intersection

--- DAG.py
+++ DAG.py
@@ -1,73 +1,88 @@
-# DAG.py
-# DAG and its related classes
-
-# NOTE:
-#   want DAG to represent many possible solutions
-#   maybe?? also want some class to represent a single solution
-#       - this class can then be "compiled" to python and returned to the user or run on other input
-
 import InputDataGraph as IDG
-
-class DAG_Edge:
-    """
-    An edge of a DAG
-
-    substr: tuple of two sets of positions
-        each set contains nodes labels from input_graph that correspond to the position
-    c_str: const string expression
-    """
-    def __init__(self, substr, input_graph, input_string, str_ind):
-        """
-        learn the DAG edge label for some substring
-
-        substr: the string to learn programs for
-        input_graph: IDG of all inputs
-        input_str: the input for the given output substring
-        str_ind: index of the input output pair in question
-        """
-
-    def intersect(self, other):
-        """
-        intersect this DAG_Edge with another
-        """
 
 class DAG:
     """
-    DAG class
+    DAG generation class
 
-    nodes: set of node labels
-    edges: dictionary mapping pairs of node labels to DAG_Edge objects
+    nodes: set of tuples representing the labels of each node
+    edges: dictionary mapping from (node1, node2) to a set of substring expressions
+                where each substring expression is (v_l, v_r, constant string)
+                v_l, v_r = independent sets of position expressions
     """
-    def __init__(self, out_str, str_ind, input_graph, input_strings, prev_DAG = None):
-        """
-        create DAG for output string given and intersect with previous DAG
 
-        out_str: output string
-        str_ind: index of the output string (used to find correct position in IDG)
-        input_graph: IDG of all inputs
-        input_strings: list of strings
-        prev_DAG: intersected DAG of all previous inputs
+    def __init__(self, input_str, output_str, input_graph, prev_DAG = None):
         """
+        create a DAG for each input-output example and intersect with the DAG of all previous examples
+        input_str: input example as string
+        output_str: corresponding output example as substring
+        input_graph: InputDataGraph generated from the whole dataset
+        prev_DAG: intersection of the DAGs of all previous examples, defaults to None
+        """
+
         self.nodes = set()
         self.edges = dict()
-        self.__generate_graph__()
+        self.generate_DAG(input_str, output_str, input_graph)
         if prev_DAG:
             self.intersect(prev_DAG)
 
-    def __generate_graph__(self, out_str, str_ind, input_graph, input_strings):
+    def generate_DAG(self, input_str, output_str, input_graph):
         """
-        generate DAG for 1 output
+        first function called when generating a DAG, creates the graph for a single input-output example
+        assumes:  nodes and edges are empty, input_str is non-empty
+        returns:  nothing
+        modifies: fills in nodes and edges as specified
         """
-        for i in range(0, len(out_str) + 1):
-            self.nodes.add((i,))
-        # generate edges
+        # get start index and end index of output string in input string
+        match = re.finditer(output_str, input_str)
+        start_index = match.start()
+        end_index = match.end()
+
+        # create len(output_str) number of nodes and a start node with label 0
         for i in range(0, len(out_str)):
-            for j in range(i, len(out_str) + 1):
-                self.edges[((i,), (j,))] = DAG_Edge(outstr[i:j], input_graph, input_strings[str_ind], str_ind)
+            self.nodes.add((i,))
 
-    def intersect(self, other):
-        """
-        intersect this DAG with 'other' DAG
-        """
+        # Iterate over all substrings output_str[i..j] of the output string and add an edge (i,j) between the labels i and j
+        for i in range(start_index, end_index - 1, 1):
+            for j in range(i + 1, end_index, 1):
+                label = ((i,), (j,))
+                self.edges.setdefault(label, set())
+                substr = in_str[i:j]
 
+                # create two independent sets of position expressions v_l and v_r
+                # a set of position expression consists of a set of IDG nodes and a constant position
+                v_l, v_r = set()
+                for v in input_graph.nodes:
+                    if i in v:
+                        v_l.add(v)
+                    if j in v:
+                        v_r.add(v)
 
+                # add constant position to v_l and v_r
+                v_l.add(i)
+                v_r.add(j)
+
+                # add independent sets of IDG nodes and constant string for given substring
+                self.edges[label].add(v_l, v_r, substr)
+
+    def intersect(self,other):
+        new_nodes = set()
+        new_edges = dict()
+        for s_edge_key in self.edges.keys():
+            for o_edge_key in other.edges.keys():
+
+                # find common nodes of both independent sets respectively
+                common = set.intersection(self.edges[s_edge_key][0], other.edges[o_edge_key][0])
+                common_vr = set.intersection(self.edges[s_edge_key][1], other.edges[o_edge_key][1])
+                if common_vr:
+                    common.add(common_vr)
+                common_str = set.intersection(self.edges[s_edge_key][2], other.edges[o_edge_key][2])
+                if common_str:
+                    common.add(common_str)
+                if common:
+                    n1 = o_edge_key[0] + s_edge_key[0]
+                    n2 = o_edge_key[1] + s_edge_key[1]
+                    new_nodes.add(n1)
+                    new_nodes.add(n2)
+                    new_edges[(n1, n2)] = common
+        self.nodes = new_nodes
+        self.edges = new_edges
